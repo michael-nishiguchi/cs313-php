4d4066c1d264ae26d88d70a3ce280669307658f3
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-01-18 22:48:43 -0700

    php

--- web/index.php
+++ web/index.php
@@ -14,6 +14,20 @@
       <?php 
         include('/common/header.php');
       ?>
+      <nav class="navbar navbar-expanded-md navbar-dark fixed-top bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="myNav">
+            <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse show" id="myNav" style>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="assignments.php">Assignments</a>
+            </li>
+        </ul>
+</nav>
         <div class="jumbotron">
           <h1 class="display-2">Michael Nishiguchi</h1>
           <p>Welcome to my website! You can see all the work I've done for CS 313 at BYU - Idaho!</p>
