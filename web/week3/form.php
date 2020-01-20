94335f57d1df2e0d0cc985b2f27cc208117d59a9
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-01-20 13:36:49 -0700

    php

---
+++ web/week3/form.php
@@ -0,0 +1,69 @@
+<?php
+
+$name = $_POST("name");
+$email = $_POST("email");
+$major = $_POST("major");
+$comments = $_POST("comments");
+
+$continents = $_POST('continents');
+
+
+//continent dictionary
+$continentDictionary = [
+    "na" => "North America",
+    "sa" => "South America",
+    "eur" => "Europe",
+    "as" => "Asia",
+    "af" => "Africa",
+    "aus" => "Australia",
+    "ant" => "Antarctica",
+];
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="main.js"></script>
+    <title>Week 3 | Teach</title>
+  </head>
+  <main>
+    <body>
+      <?php 
+        include 'header.php';
+      ?>
+      
+    <div class="jumbotron">
+          <h1 class="display-2">Week 3 - Teach: Team Activity</h1>
+    </div>
+
+    <div class="container">
+
+        <h1>Name: <?php echo $name ?></h1>
+        <p>Email: <?php echo "<a href='mailto:'" . $email . ">" . $email . "</a>" ?></p>
+        <p>Major: <?php echo $major ?></p>
+        <p>Comments: <?php echo $comments ?></p>
+
+        <p>Continents Visited:<p>
+        <br>
+        <ul>
+        <?php   
+                // Loop to store and display values of individual checked checkbox.
+                foreach($continents as $selected){
+                echo "<li>" . $continentDictionary[$selected] . "</li>";
+        ?>
+        </ul>
+    
+    </div>
+
+    
+    
+    </main>
+  </body>
+</html>
---
+++ web/week3/teach.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="main.js"></script>
+    <title>Week 3 | Teach</title>
+  </head>
+  <main>
+    <body>
+      <?php 
+        include 'header.php';
+      ?>
+      
+    <div class="jumbotron">
+          <h1 class="display-2">Week 3 - Teach: Team Activity</h1>
+    </div>
+
+    <div class="container">
+        <form action="form.php" method="POST">
+            <label for="name">Name</label>
+            <input type="text" name="name">
+
+            <label for="email">Email</label>
+            <input type="text" name="email">
+
+            <?php 
+                $majors = array("Computer Science", "Web Design and Development", "Computer Information Technology", "Computer Engineering");
+
+                foreach ($majors as $val) {
+                  echo '<input type="radio" name="major" value=' . $val . '>' . $val . '<br>';
+                }
+            ?> 
+            <textarea placeholder="Comments..."></textarea>
+
+            <input type="checkbox" name="continent[]" value="na">North America</input>
+            <input type="checkbox" name="continent[]" value="sa">South America</input>
+            <input type="checkbox" name="continent[]" value="eur">Europe</input>
+            <input type="checkbox" name="continent[]" value="as">Asia</input>
+            <input type="checkbox" name="continent[]" value="af">Africa</input>
+            <input type="checkbox" name="continent[]" value="aus">Australia</input>
+            <input type="checkbox" name="continent[]" value="ant">Antarctica</input>
+
+
+            <input type="submit">
+        </form>
+
+    </div>
+
+    
+    
+    </main>
+  </body>
+</html>
