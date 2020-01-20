a428aff11f62b9dcc0ad43d2d5d82a8e758c7588
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-01-20 13:59:42 -0700

    php

--- web/week3/form.php
+++ web/week3/form.php
@@ -29,7 +29,7 @@ $continentDictionary = [
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../main.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="main.js"></script>
@@ -58,7 +58,7 @@ $continentDictionary = [
         <?php   
 
             // Loop to store and display values of individual checked checkbox.
-            foreach($continents as $selected => $value){
+            foreach($continents as $key => $value){
             echo "<li>" . $value . "</li>";
 
 
