9b371a9bb71214e71b883c2ec78cc7933b61c1b2
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-04 11:16:28 -0700

    week 5 teach

--- web/assignments.php
+++ web/assignments.php
@@ -21,7 +21,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-4">
-            <a href="hello.html">Wk 2 Assignment</a>
+            <a href="week2/hello.html">Wk 2 Assignment</a>
             <br>
             <a href="week3/teach.php">Wk 3 Assignment</a>
             <br>
--- web/week5/index.php
+++ web/week5/index.php
@@ -14,9 +14,9 @@
     $stmt = $db->prepare($query);
     $stmt->execute();
 
+    
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
-    var_dump($row);
 	// The variable "row" now holds the complete record for that
 	// row, and we can access the different values based on their
 	// name
