01e8d96365c6651fdf585c2c5326c3bcc9db7e83
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-04 09:43:39 -0700

    week 4 teach

--- web/week4/index.php
+++ web/week4/index.php
@@ -10,16 +10,31 @@
     require("dbConnect.php");
     // DB assigned to db variable
     $db = get_db();
-    $query = "SELECT * FROM Scriptures";
+    $query = "SELECT book, chapter, verse, content FROM Scriptures";
     $stmt = $db->prepare($query);
     $stmt->execute();
 
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	// The variable "row" now holds the complete record for that
+	// row, and we can access the different values based on their
+	// name
+	$book = $row['book'];
+	$chapter = $row['chapter'];
+	$verse = $row['verse'];
+	$content = $row['content'];
+
+	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+}
+
+    /*
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     foreach ($rows as $value) {
         echo $value;
         //echo "<p>" . $value['book'] . " " . $value['chapter'] . ":" . $value['verse'] . " - \"" . $value['content'] . "\"</p>";
     }
+    */
 ?>
 </body>
 
