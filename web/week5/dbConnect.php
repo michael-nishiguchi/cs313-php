55cbefc2d4b2ecad50de996465cbbcdaad66b836
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-04 10:54:50 -0700

    week 5 teach

--- web/week5/dbConnect.php
+++ web/week5/dbConnect.php
@@ -1,22 +1,23 @@
 <?php 
-   function get_db() {
-    
-      $db = null;
-      try {
-		 //$dbUrl = getenv('DATABASE_URL');
-		 $dbUrl = 'postgres://juasbzbxoiigrs:663d20a3005312fced32656243cf67d498a4a10e2eaf20fbf5ca5828729223e3@ec2-174-129-33-14.compute-1.amazonaws.com:5432/d4i3elnh9g0rfi';
-         $dbOpts = parse_url($dbUrl);
-         $dbHost = $dbOpts["host"];
-         $dbPort = $dbOpts["port"];
-         $dbUser = $dbOpts["user"];
-         $dbPassword = $dbOpts["pass"];
-         $dbName = ltrim($dbOpts["path"],'/');
-         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);         
-      }
-      catch (PDOException $ex) {
-         echo "Connection error.";
-         die();
-      }
-      return $db;
-   }
+	function get_db() {
+
+		$db = NULL;
+		try {
+			$dbUrl = getenv('DATABASE_URL');
+			$dbOpts = parse_url($dbUrl);
+			var_dump($dbUrl);
+					
+			$dbHost = $dbOpts["host"];
+			$dbPort = $dbOpts["port"];
+			$dbUser = $dbOpts["user"];
+			$dbPassword = $dbOpts["pass"];
+			$dbName = ltrim($dbOpts["path"],'/');
+			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);         
+		}
+		catch (PDOException $ex) {
+			echo "Connection error.";
+			die();
+		}
+			return $db;
+	}
 ?>
