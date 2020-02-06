5dd551b4872d50dffc003ae1019b9798d20aa09b
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-06 14:01:08 -0700

    Project queries.php

--- web/project1/queries.php
+++ web/project1/queries.php
@@ -24,17 +24,3 @@ function getAllUsers() {
     $stmt->closeCursor();
     return $stmt;
 }
-
-function getAllCat    $db = get_db();
-$sql = 'INSERT INTO category
-            (category_name, amount_budgeted)
-        VALUES (:categoryName, :amountBudgeted)';
-$stmt = $db->prepare($sql);
-$stmt->bindValue(':categoryName', $categoryName, PDO::PARAM_STR);
-$stmt->bindValue(':amountBudgeted', $amountBudgeted, PDO::PARAM_STR);
-$stmt->execute();
-$rowsChanged = $stmt->rowCount();
-$stmt->closeCursor();
-return $rowsChanged;egory() {
-    
-}
--- web/project1/view-budget.php
+++ web/project1/view-budget.php
@@ -1,4 +1,5 @@
 <?php
+include('queries.php');
 $stmt = getAllUsers();
 
 ?>
