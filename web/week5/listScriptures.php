9aa153b47cc10d6564022cc6b1a01bbffe9d1b12
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-10 14:18:48 -0700

    Week 6 teach

--- web/week5/listScriptures.php
+++ web/week5/listScriptures.php
@@ -9,6 +9,25 @@
 <?php
     require("dbConnect.php");
     $db = get_db();
+
+    $book = strip_tags($_POST["book"]);
+    $chapter = strip_tags($_POST["chapter"]);
+    $verse = strip_tags($_POST["verse"]);
+    $content = strip_tags($_POST["content"]);
+    $topics = $_POST("topics[]");
+    
+    $query = "INSERT INTO Scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content);";
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":book", $book, PDO::PARAM_STR);
+    $stmt->bindValue(":chapter", $chapter, PDO::PARAM_INT);
+    $stmt->bindValue(":verse", $verse, PDO::PARAM_INT);
+    $stmt->bindValue(":content", $content, PDO::PARAM_STR);
+    $stmt->execute();
+
+    
+
+
+
     $query = "SELECT book, chapter, verse, content FROM Scriptures";
     
     $stmt = $db->prepare($query);
