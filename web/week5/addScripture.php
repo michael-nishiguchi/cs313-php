c19d2fa7e46a572ad8df7e33ed2efb5686b231dd
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-10 13:51:23 -0700

    Week 6 teach

--- web/week5/addScripture.php
+++ web/week5/addScripture.php
@@ -6,17 +6,7 @@
 
 <body>
 
-<?php
-    require("dbConnect.php");
-    // DB assigned to db variable
-    $db = get_db();
-    $query = "SELECT book, chapter, verse, content FROM Scriptures";
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-
-?>
-
-<form>
+<form action="listScriptures.php" method="post">
     <label for="book">Book</label>
     <input type="text" name="book">
 
@@ -29,15 +19,19 @@
     <label for="content">Content</label>
     <textarea name="content" rows="5"></textarea>
 
+    <ul>
     <?php 
+        require("dbConnect.php");
         $query = "SELECT topic_name FROM topic;";
-        $stmt = db->prepare($query);
+        $stmt = $db->prepare($query);
         $stmt->execute();
         
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<input type='checkbox' "
+            $topic = $row["topic_name"];
+            echo "<li><input type='checkbox' name='topic[]' value=$topic>$topic</li>";
         }
     ?>
+    </ul>
 </form>
 </body>
 
