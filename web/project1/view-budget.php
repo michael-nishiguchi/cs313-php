b5c76bf91e3edd4ea506dcd7206fc236585dd474
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-15 21:39:14 -0700

    Project 1 login

--- web/project1/view-budget.php
+++ web/project1/view-budget.php
@@ -2,7 +2,7 @@
 require_once 'queries.php';
   if ($_SESSION['loggedin']) {
       $transactions = getTransactionsFromId($_SESSION('user_id'));
-      $categories = getCategoriesFromId($_SESSION('user_id'));
+      //$categories = getCategoriesFromId($_SESSION('user_id'));
     exit;
   } 
   else {
