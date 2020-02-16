05370d66bd9f2107562650ecad8d548c6bac7dda
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-15 19:01:31 -0700

    Project 1 login

--- web/project1/login.php
+++ web/project1/login.php
@@ -19,17 +19,13 @@ $transactions = getAllTransactions();
 <body class="text-center">
 
 <form class="form-signin">
-      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" data-kwimpalastatus="alive" data-kwimpalaid="1581817532009-4">
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" data-kwimpalastatus="alive" data-kwimpalaid="1581817532009-3">
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
+
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 </form>
 
