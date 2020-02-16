b88a086ad89a96451392f16845fe192b9ab28f90
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-15 22:05:02 -0700

    Project 1 login

--- web/project1/login.php
+++ web/project1/login.php
@@ -14,7 +14,8 @@
 <form class="form-signin" action="index.php" method="post">
     <?php
         if(isset($message)){
-            echo $message;
+            //echo $message;
+            var_dump($message);
         }
     ?>
 
