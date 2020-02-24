fc079b3aaaa4038ed3413bc44b53c25e9f8ad983
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-24 16:04:57 -0700

    Project 1 login

--- web/project1/view-budget.php
+++ web/project1/view-budget.php
@@ -46,9 +46,29 @@ require_once 'queries.php';
           <!--modal call -->
           <td scope="col">
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".viewModal">View</button>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".deleteModal">Delete</button>
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target=".deleteModal">Delete</button>
           </td>
-          
+
+          <div class="deleteModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!--
           <div class="modal fade viewModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
               <div class="modal-content">
@@ -56,6 +76,7 @@ require_once 'queries.php';
               </div>
             </div>
           </div>
+          -->
           <div class="modal fade deleteModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
               <div class="modal-content">
