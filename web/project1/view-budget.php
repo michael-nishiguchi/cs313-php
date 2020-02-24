1d8dc718440f25c9246ae760275a867bbef41fe7
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2020-02-23 17:45:19 -0700

    Project 1 login

--- web/project1/view-budget.php
+++ web/project1/view-budget.php
@@ -3,11 +3,11 @@ require_once 'queries.php';
   if ($_SESSION['loggedin'] == TRUE) {
     
     $transactions = getTransactionsFromId($_SESSION['user_id']);
-    /*
+    
     while($row = $transactions->fetchAll(PDO::FETCH_ASSOC){
       var_dump($row);
     }
-    */
+    
     var_dump($transactions);
 
     //$categories = getCategoriesFromId($user_id);
